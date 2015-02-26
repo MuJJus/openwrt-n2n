7a9e680aeb74e517a4bcd8cff0ac288d0ae63687
MuJJus <jason.tse@qq.com>
2015-02-26 01:01:02 +0000

    init

---
+++ Makefile
@@ -0,0 +1,43 @@
+
+include $(TOPDIR)/rules.mk
+
+PKG_BRANCH:=trunk
+PKG_SOURCE_URL:=https://svn.ntop.org/svn/ntop/trunk/n2n/n2n_v2
+PKG_REV:=9015
+
+PKG_NAME:=n2n-edge
+PKG_VERSION:=2.1_svn$(PKG_REV)
+PKG_RELEASE:=1
+PKG_LICENSE:=GPLv3
+
+PKG_SOURCE_SUBDIR:=$(PKG_NAME)-$(PKG_VERSION)
+PKG_SOURCE:=$(PKG_SOURCE_SUBDIR).tar.gz
+PKG_SOURCE_PROTO:=svn
+PKG_SOURCE_VERSION:=$(PKG_REV)
+
+include $(INCLUDE_DIR)/package.mk
+
+define Package/n2n-edge
+  SECTION:=net
+  CATEGORY:=Network
+  TITLE:=N2N VPN tunneling daemon(V2)
+  URL:=http://www.ntop.org/n2n/
+  SUBMENU:=VPN
+  DEPENDS:=+libpthread +kmod-tun +libopenssl +resolveip
+endef
+
+define Build/Compile
+	$(MAKE) -C $(PKG_BUILD_DIR) \
+		$(TARGET_CONFIGURE_OPTS) \
+		CFLAGS="$(TARGET_CFLAGS)" \
+		INSTALL_PROG=":"
+endef
+
+define Package/n2n-edge/install
+	$(INSTALL_DIR) $(1)/usr/sbin
+	$(INSTALL_BIN) $(PKG_BUILD_DIR)/edge $(1)/usr/sbin/
+	$(INSTALL_DIR) $(1)/lib/netifd/proto
+	$(INSTALL_BIN) ./files/n2n.sh $(1)/lib/netifd/proto
+endef
+
+$(eval $(call BuildPackage,n2n-edge))
---
+++ files/n2n.sh
@@ -0,0 +1,63 @@
+#!/bin/sh
+
+[ -n "$INCLUDE_ONLY" ] || {
+        . /lib/functions.sh
+        . ../netifd-proto.sh
+        init_proto "$@"
+}
+
+proto_n2n_setup() {
+        local cfg="$1"
+        local device="n2n-$cfg"
+        local supernodehost supernodeport mode ipaddr netmask mac mtu community key forwarding
+        json_get_vars supernodehost supernodeport mode ipaddr netmask mac mtu community key forwarding
+
+        [ -n "$supernodehost" ] && {
+                for ip in $(resolveip -t 5 "$supernodehost"); do
+                        ( proto_add_host_dependency "$cfg" "$ip" )
+                        serv_addr=1
+                done
+        }
+        [ -n "$serv_addr" ] || {
+                echo "Could not resolve server address"
+                sleep 5
+                proto_setup_failed "$cfg"
+                exit 1
+        }
+
+        proto_run_command "$cfg" /usr/sbin/edge -f -d "$device" -l "${supernodehost}:${supernodeport}" -a "${mode}:${ipaddr}" -s "$netmask" -c "$community" $([ -n "$key" ] && echo -k $key) $([ -n "$mac" ] && echo -m $mac) $([ -n "$mtu" ] && echo -M $mtu) $([ -n "$forwarding" ] && echo -r)
+
+        proto_init_update "$device" 1 1
+        proto_set_keep 1
+        sleep 1
+        proto_send_update "$cfg"
+}
+
+proto_n2n_teardown() {
+        local cfg="$1"
+        local device="n2n-$cfg"
+
+        proto_init_update "$device" 0
+        proto_kill_command "$1"
+        proto_send_update "$cfg"
+}
+
+proto_n2n_init_config() {
+        no_device=1
+        available=1
+
+        proto_config_add_string "supernodehost"
+        proto_config_add_int "supernodeport"
+        proto_config_add_string "mode"
+        proto_config_add_string "ipaddr"
+        proto_config_add_string "netmask"
+        proto_config_add_string "mac"
+        proto_config_add_int "mtu"
+        proto_config_add_string "community"
+        proto_config_add_string "key"
+        proto_config_add_boolean "forwarding"
+}
+
+[ -n "$INCLUDE_ONLY" ] || {
+        add_protocol n2n
+}
